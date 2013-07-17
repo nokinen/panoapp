e4bb13e1f169e1e8a4074ce477ee78dcb6f1aeaa
Tobias Noiges <tobias@noig.es>
2013-07-16 20:13:14 -0700

    Add thin, use strong parameters

--- app/controllers/panoramas_controller.rb
+++ app/controllers/panoramas_controller.rb
@@ -1,23 +1,29 @@
 class PanoramasController < ApplicationController
-  
+
   def index
     @panoramas = Panorama.all order: "created_at DESC"
   end
-  
+
   def create
-    @panorama = Panorama.new params[:panorama]
+    @panorama = Panorama.new panorama_params
     @panorama.save ? head(:created, location: @panorama) : head(:bad_request)
   end
-  
+
   def show
     @panorama = Panorama.find params[:id]
   end
-  
+
   def destroy
     @panorama = Panorama.find params[:id]
     @panorama.destroy
-    
+
     render(nothing: true, status: 204)
   end
+
+  private
+
+    def panorama_params
+      params.require(:panorama).permit(:fullsize, :preview)
+    end
 
 end
