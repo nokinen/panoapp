42a32333a810ea0cf07b93b26c14e68c7bc3e2b2
Tobias Noiges <tobias@noig.es>
2013-07-16 19:34:45 -0700

    Remove deprecation warnings

--- app/models/panorama.rb
+++ app/models/panorama.rb
@@ -1,5 +1,4 @@
 class Panorama < ActiveRecord::Base
-  attr_accessible :fullsize, :preview
   validates :fullsize, presence: true, format: { with: URI::regexp(%w(http https)) }
   validates :preview, presence: true, format: { with: URI::regexp(%w(http https)) }
 end
